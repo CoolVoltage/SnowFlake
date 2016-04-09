b1ba6bd3e0c4253604302b21dade7130b376084d
Karthik Vijay <vijaypcguy@gmail.com>
2016-04-09 07:51:04 -0400

    set new password for vm

--- instance/scripts/startVM.sh
+++ instance/scripts/startVM.sh
@@ -1,4 +1,12 @@
+
 containerId=`docker run -d -P eg_sshd`
 echo $containerId
+
 port=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "22/tcp") 0).HostPort}}' $containerId)
 echo $port
+
+name=$(docker inspect -f '{{.Name}}' $containerId)
+name=$(echo $name | cut -d '/' -f 2)
+echo $name
+
+echo "root:$name" | docker exec -i $containerId chpasswd -
