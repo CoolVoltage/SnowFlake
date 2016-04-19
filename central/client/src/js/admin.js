b0090556c5cca16c59e8c6fb01bf82a9495f6767
Sriram Sundarraj <ssundarraj@gmail.com>
2016-04-19 13:24:31 +0530

    Add admin UI idle vm
    Signed-off-by: Sriram Sundarraj <ssundarraj@gmail.com>

--- central/client/src/js/admin.js
+++ central/client/src/js/admin.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         url: "http://localhost:8000/login.html",
         context: document.body
     }).done(function(data) {
-        data = '{ "message":"success", "instances": [ { "id": 2, "ip": "127.0.0.1", "owner": "1", "created_at": "2016-04-17 19:15:19", "updated_at": "2016-04-17 19:15:57", "password": "lol" } ], "virtualMachines": [ { "id": 1, "unique_identifier": "sddfhdfdfs", "running": 1, "ip": "127.0.0.1", "port": "32807", "owner": "1", "created_at": "2016-04-06 00:00:00", "updated_at": "2016-04-14 00:00:00" } ] }';  // Just for testing
+        data = '{ "message":"success", "instances": [ { "id": 2, "ip": "127.0.0.1", "owner": "1", "created_at": "2016-04-17 19:15:19", "updated_at": "2016-04-17 19:15:57", "password": "lol" } ], "virtualMachines": [ { "id": 1, "unique_identifier": "sddfhdfdfs", "running": 0, "ip": "127.0.0.1", "port": "32807", "owner": "1", "created_at": "2016-04-06 00:00:00", "updated_at": "2016-04-14 00:00:00" }, { "id": 1, "unique_identifier": "sddfhdfdfs", "running": 1, "ip": "127.0.0.1", "port": "32807", "owner": "1", "created_at": "2016-04-06 00:00:00", "updated_at": "2016-04-14 00:00:00" } ] }';  // Just for testing
         data = JSON.parse(data);
         if(data['message'] == "success"){
             var instanceData = data['instances'];
@@ -13,13 +13,13 @@ $(document).ready(function() {
             instanceData.forEach(function(element, index, array){
                 console.log(element);
                 var instanceCard = document.createElement("div");
-                instanceCard.className = "ui container clearing segment text cards"
+                instanceCard.className = "ui container clearing segment text cards padded";
                 instanceCard.innerHTML += "IP: " + element['ip'] + "<br/>";
                 instanceCard.innerHTML += "Owner: " + element['owner'] + "<br/>";
                 var vmDiv = document.createElement("div");
                 vmDiv.className = "ui cards clearing";
                 preemtableData.forEach(function(vmElement, index, array){
-                    if (vmElement['ip'] == element['ip']){
+                    if (vmElement['ip'] == element['ip'] && vmElement['running']){
                         var oneVMDiv = document.createElement("div");
                         oneVMDiv.className = "card";
                         oneVMDiv.innerHTML += "unique_identifier: " + vmElement['unique_identifier'] + "<br/>";
@@ -32,25 +32,21 @@ $(document).ready(function() {
                 adminDiv.appendChild(instanceCard);
 
             });
-            // preemtableData.forEach(function(element, index, array){
-            //     var instanceRow = document.createElement("tr");
-            //     var idCol = document.createElement("td");
-            //     var runningCol = document.createElement("td");
-            //     var ipCol = document.createElement("td");
-            //     var ctCol = document.createElement("td");
-            //     var passCol = document.createElement("td");
-            //     idCol.innerHTML = element['unique_identifier'];
-            //     runningCol.innerHTML = element['running'] ? "Yes" : "No";
-            //     ipCol.innerHTML = element['ip'];
-            //     ctCol.innerHTML = element['created_at'];
-            //     passCol.innerHTML = element['password'];
-            //     instanceRow.appendChild(idCol);
-            //     instanceRow.appendChild(runningCol);
-            //     instanceRow.appendChild(ipCol);
-            //     instanceRow.appendChild(ctCol);
-            //     instanceRow.appendChild(passCol);
-            //     document.getElementById('preemtableTableBody').appendChild(instanceRow);
-            // });
+            adminDiv.innerHTML += "<br /><br />"
+            var idleCard = document.createElement("div");
+            idleCard.className = "ui container clearing segment text cards padded";
+            idleCard.innerHTML = "Idle VMs<br/><br/>";
+            preemtableData.forEach(function(element, index, array){
+                if(!element['running']){
+                    console.log("Idle");
+                    var oneIdleVM = document.createElement("div");
+                    oneIdleVM.className = "red card";
+                    oneIdleVM.innerHTML += "unique_identifier: " + element['unique_identifier'] + "<br/>";
+                    oneIdleVM.innerHTML += "owner: " + element['owner'] + "<br/>";
+                    idleCard.appendChild(oneIdleVM);
+                }
+            });
+            adminDiv.appendChild(idleCard);
         }
         else{
             
